35d7574576ffa0e7e09349fbd334123ddfa37f00
students <anmihajlov@beleriand.yandex.ru>
2018-06-10 00:41:30 +0300

    fix time

--- gen_command.py
+++ gen_command.py
@@ -1,6 +1,6 @@
 with open('command.txt', 'w') as f:
     comm=[]
-    gpus = [0, 1, 2, 3, 4, 5, 6, 7]
+    gpus = [0, 1, 2, 3]
     idx = 0
     cur_l = 'a'
     for i in range(len(gpus)):
@@ -12,8 +12,11 @@ with open('command.txt', 'w') as f:
         idx += 1
         cur_l = chr(ord(cur_l) + 1)
 
-    command = '[\'./engines/lc0_test\', \'--slowmover=4.2\', \'--temperature=0.5\', \'--tempdecay-moves=5\', \'--threads=20\', \'--weights=engines/weights_359.txt.gz\', \'--cpuct=3.6\', \'--fpu-reduction=0.85\', \'--backend=multiplexing\', \'--move-overhead=70\', \'--policy-softmax-temp=2.3\', \'--allowed-node-collisions=32\', \'--backend-opts=' + ','.join(comm) + '\']'
+    command = '[\'/home/anmihajlov/lichess/youngleela/engines/lc0_test\', \'--temperature=0.5\', \'--tempdecay-moves=5\', \'--threads=20\', \'--weights=/home/anmihajlov/lichess/youngleela/engines/weights_27.txt.gz\', \'--cpuct=3.6\', \'--fpu-reduction=0.85\', \'--backend=multiplexing\', \'--move-overhead=70\', \'--policy-softmax-temp=2.3\', \'--allowed-node-collisions=32\', \'--backend-opts=' + ','.join(comm) + '\']'
     print(command, file=f)
 
+command = eval(command)
+command[-1] = '\"' + command[-1] + '\"'
+
 with open('run_command.sh', 'w') as f:
-    print(' '.join(eval(command)), file=f)
+    print(' '.join(command), file=f)
